337e14a0dcb0d5a676fa6a4951f890ef9a69a647
Dylan Neve <dylanneve1@gmail.com>
2025-02-25 10:49:04 +0000

    fix the subtraction

--- LabE.srcs/sources_1/imports/adder.srcs/sources_1/new/6bit_ripple_adder.v
+++ LabE.srcs/sources_1/imports/adder.srcs/sources_1/new/6bit_ripple_adder.v
@@ -23,12 +23,16 @@
 module _6bit_ripple_adder (input [5:0] x, y, input sel, output overflow, c_out, output [5:0] sum);
     wire c1, c2, c3, c4, c5, c6;
     
-    FullAdder bit0 (x[0], y[0], sel, sum[0], c1);
-    FullAdder bit1 (x[1], y[1], c1, sum[1], c2);
-    FullAdder bit2 (x[2], y[2], c2, sum[2], c3);
-    FullAdder bit3 (x[3], y[3], c3, sum[3], c4);
-    FullAdder bit4 (x[4], y[4], c4, sum[4], c5);
-    FullAdder bit5 (x[5], y[5], c5, sum[5], c6);
+    wire [5:0] y_processed;
+    
+    assign y_processed = sel ? ~y : y;
+        
+    FullAdder bit0 (x[0], y_processed[0], sel, sum[0], c1);
+    FullAdder bit1 (x[1], y_processed[1], c1, sum[1], c2);
+    FullAdder bit2 (x[2], y_processed[2], c2, sum[2], c3);
+    FullAdder bit3 (x[3], y_processed[3], c3, sum[3], c4);
+    FullAdder bit4 (x[4], y_processed[4], c4, sum[4], c5);
+    FullAdder bit5 (x[5], y_processed[5], c5, sum[5], c6);
     
     xor g0 (overflow, c5, c6);
     assign c_out = c6;
